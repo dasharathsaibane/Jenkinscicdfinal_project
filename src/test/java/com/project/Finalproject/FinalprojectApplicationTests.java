cd897b8827ee296761dfc3741a901ce415546f42
dasharathsaibane <dasharathsaiabane@gmail.com>
2021-12-15 21:40:51 +0530

    first commit

---
+++ src/test/java/com/project/Finalproject/FinalprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.Finalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
